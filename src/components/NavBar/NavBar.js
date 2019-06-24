158a634c8c45918f712730ea6f4c6223b40197d1
Heriberto Uroza <heribertouroza@pursuit.org>
2019-06-24 18:01:37 -0400

    icon added, planned out tabs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import vid_player from '../../icons/video_player.png'
 import M from 'materialize-css'
 
 class NavBar extends Component {
@@ -11,36 +12,40 @@ class NavBar extends Component {
     render() {
         return (
             <>
-                <nav class="nav-extended">
-                    <div class="nav-wrapper">
-                        <a href="#" class="brand-logo">Heri-Tube</a>
-                        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <nav className="nav-extended">
+                    <div className="nav-wrapper" style={{ textAlign: 'center', paddingRight: '60px' }}>
+                        <Link path="/home" ><img src={vid_player} alt='vid_player' style={{maxHeight: '40px'}}/></Link>
+                        <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
                             <li><a href="sass.html">Sass</a></li>
                             <li><a href="badges.html">Components</a></li>
                             <li><a href="collapsible.html">JavaScript</a></li>
                         </ul>
                     </div>
-                    <div class="nav-content">
-                        <ul class="tabs tabs-transparent">
-                            <li class="tab"><a href="#test1">Test 1</a></li>
-                            <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-                            <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
-                            <li class="tab"><a href="#test4">Test 4</a></li>
+                    <div className="nav-content">
+                        <ul className="tabs tabs-transparent">
+                            <li className="tab"><Link path="/add">Add</Link></li>
+                            <li className="tab"><a className="active" href="#test2">Test 2</a></li>
+                            <li className="tab disabled"><a href="#test3">Disabled Tab</a></li>
+                            <li className="tab"><a href="#test4">Test 4</a></li>
+                            <li className="tab"><a href="#test1">Test 1</a></li>
+                            <li className="tab"><a className="active" href="#test2">Test 2</a></li>
+                            <li className="tab disabled"><a href="#test3">Disabled Tab</a></li>
+                            <li className="tab"><a href="#test4">Test 4</a></li>
                         </ul>
                     </div>
                 </nav>
 
-                <ul class="sidenav" id="mobile-demo">
+                <ul className="sidenav" id="mobile-demo">
                     <li><a href="sass.html">Sass</a></li>
                     <li><a href="badges.html">Components</a></li>
                     <li><a href="collapsible.html">JavaScript</a></li>
                 </ul>
 
-                <div id="test1" class="col s12">Test 1</div>
-                <div id="test2" class="col s12">Test 2</div>
-                <div id="test3" class="col s12">Test 3</div>
-                <div id="test4" class="col s12">Test 4</div>
+                <div id="test1" className="col s12">Test 1</div>
+                <div id="test2" className="col s12">Test 2</div>
+                <div id="test3" className="col s12">Test 3</div>
+                <div id="test4" className="col s12">Test 4</div>
             </>
         )
     }
