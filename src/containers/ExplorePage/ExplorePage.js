4533d2ca3b8135d3b6f752490121765e8c355eb3
Heriberto Uroza <heribertouroza@pursuit.org>
2019-07-28 11:33:39 -0400

    rewrote some materialize styling to fit my own

--- src/containers/ExplorePage/ExplorePage.js
+++ src/containers/ExplorePage/ExplorePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './explorepage.css'
 
 class ExplorePage extends Component {
 
@@ -7,7 +8,7 @@ class ExplorePage extends Component {
             <>
                 <div className='search'>
                     <form>
-                        <input className='search_input'></input>
+                        <input className='search_input' placeholder='Eplore Videos Here!'></input>
                     </form>
                 </div>
                 
