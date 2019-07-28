3d4addfd58ec113ea7c57540624e6b64a66b2024
Heriberto Uroza <heribertouroza@pursuit.org>
2019-07-28 11:15:08 -0400

    added input

---
+++ src/containers/ExplorePage/ExplorePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ExplorePage extends Component {
+
+    render() {
+        return (
+            <>
+                <div className='search'>
+                    <form>
+                        <input className='search_input'></input>
+                    </form>
+                </div>
+                
+            </>
+        )
+    }
+}
+
+export default ExplorePage;
