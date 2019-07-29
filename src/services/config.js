d8db54d997bd865350c8f54626d8137e6d7c3cb7
Heriberto Uroza <heribertouroza@pursuit.org>
2019-07-29 19:26:32 -0400

    added get request from youtube api

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 const ytKey = {
-    "REACT_APP_API_KEY": process.env.REACT_APP_API_KEY
+    REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
 
 export { ytKey }
---
+++ src/services/functions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { ytKey } from './config'
+
+const youTubeAPI = (searchQuery) => {
+    axios({
+            method: 'get',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            params: {
+                part: 'snippet',
+                maxResults: 8,
+                videoDefinition: 'high',
+                type: 'video',
+                videoEmbeddable: 'true',
+                key: ytKey.REACT_APP_API_KEY,
+                q: searchQuery,
+                pageToken: ''
+            }
+        })
+        .catch(err => {
+            console.log(err.toString())
+        })
+}
